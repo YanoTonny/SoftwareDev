14f4ad43b3e02c07a8bd43158b39f019bf64429c
Tonny Yano <136583143+YanoTonny@users.noreply.github.com>
2024-02-28 20:57:19 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main(){
+  print("Hello World, my name is Tonny and this is my first dart code.");
+  // Declaring variables
+  String name = "Tonny Odhiambo";
+  String address = "Nairobi, Kenya";
+  num age = 25;
+  num height = 6.1;
+  bool isMarried = false;
+
+  print(name);
+  print(address);
+  print(age);
+  print(height);
+  print(isMarried);
+}
