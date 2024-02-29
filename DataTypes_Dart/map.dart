2259e818364ff3fe69ecb53ee31e51e655c959e1
YanoTonny <yano.odhiambo@gmail.com>
2024-02-29 13:12:58 +0300

    Initial commit

---
+++ DataTypes_Dart/map.dart
@@ -0,0 +1,6 @@
+void main(){
+  Map<String, int> ages= {'Tonny Yano' : 26, 'Hillary Omondi' : 26, 'James Omenda' : 26};
+  Map<String, int> address= {'Nairobi' : 47, 'Migori' : 44, 'Homabay' : 41};
+  print("Ages of students: $ages");
+  print("Addreses of towns: $address");
+}
