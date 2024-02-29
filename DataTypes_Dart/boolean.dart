351a957d165bce51b7760a5fd175c6f7b9993e3d
YanoTonny <yano.odhiambo@gmail.com>
2024-02-29 12:31:36 +0300

    Initial commit

---
+++ DataTypes_Dart/boolean.dart
@@ -0,0 +1,8 @@
+void main(){
+  bool isMarried = false;
+  bool isStudent = true;
+  bool isChristian = true;
+  bool IsHappy = true;
+  print("Married status: $isMarried\nStudent status: $isStudent\nChristian status: $isChristian\nHappy status:$IsHappy"
+  );
+}
