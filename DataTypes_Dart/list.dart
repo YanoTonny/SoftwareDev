58d9c7df4f03ec480c2080995a27d705dae4e9e4
YanoTonny <yano.odhiambo@gmail.com>
2024-02-29 12:52:22 +0300

    Second commit

--- DataTypes_Dart/list.dart
+++ DataTypes_Dart/list.dart
@@ -1,8 +1,6 @@
-import 'dart:svg';
-
 void main(){
   List<String> names = ["Tonny Yano","James Omenda","Hillary Omondi"];
-  List<String> fruits = ["Apple","Mangoe","Oranges","Avocado","Jack fruit"];
+  List<String> fruits = ["Apple","Mangoes","Oranges","Avocado","Jack fruit"];
   List<int> numbers =[1,2,3,4,5,6,7,8,9,10];
   print("${names}\n${fruits}\n${numbers}");
   // print(names);
