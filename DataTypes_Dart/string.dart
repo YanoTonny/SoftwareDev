77a4e73eafa16298b80d93b235eaaf500bb64370
YanoTonny <yano.odhiambo@gmail.com>
2024-02-29 12:04:12 +0300

    Initial commit

---
+++ DataTypes_Dart/string.dart
@@ -0,0 +1,11 @@
+void main() {
+// Declaring Values     
+String StudentName = "Tonny Odhiambo Yano";
+String schoolName = "Powerlearn Project Academy";
+String address = "Nairobi,Kenya";
+String mood = "I am very happy to be learning how to code in dart";   
+
+// Printing Values
+print("My name is $StudentName. My School name is $schoolName and the address is $address");   
+print(mood);
+}
