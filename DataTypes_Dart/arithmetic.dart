9122a7dbba3232570c5e9d151d83c5c0551a9f16
YanoTonny <yano.odhiambo@gmail.com>
2024-02-29 20:13:32 +0300

    Initial commit

---
+++ DataTypes_Dart/arithmetic.dart
@@ -0,0 +1,25 @@
+void main(){
+  // Declaring five numbers
+  int num1 = 65;
+  int num2 = 54;
+  int num3 = 32;
+  double num4 = 78.224;
+  double num5 = 34.6657;
+  // Performing calculations
+  int sum= num1+num2+num3; //addition
+  double subtraction= num4-num5; //unary minus
+  int diff= num2-num3; // subraction
+  int mul= num1*num2*num3; //multiplication
+  int div= num1~/num3; //integer division
+  double div1 = num1/num2; //division
+  int mod = num1%num2; // show remainder
+  // printing
+  print("The addition is $sum");
+  print("The unary minus is $subtraction");
+  print("The subrtraction is $diff ");
+  print("The multiplication is $mul");
+  print("The integer division is $div");
+  print("The division is $div1");
+  print("The show remainder is $mod");
+
+}
