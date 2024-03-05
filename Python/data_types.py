d7db9d9aac47ea83d6c52110badecc5cc4e6270e
YanoTonny <yano.odhiambo@gmail.com>
2024-03-05 20:49:13 +0300

    Reorganization of files into folders based on programming language

---
+++ Python/data_types.py
@@ -0,0 +1,5 @@
+c=144.346
+d=123+5j
+print("The type of variable having value", c, "is", type(c))
+print("The type of variable having value", d, "is", type(d))
+print(c,d)
--- data_types.py
+++
@@ -1,5 +0,0 @@
-c=144.346
-d=123+5j
-print("The type of variable having value", c, "is", type(c))
-print("The type of variable having value", d, "is", type(d))
-print(c,d)
