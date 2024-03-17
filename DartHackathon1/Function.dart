08378af819c6745dfbff1b7fa9d826fa6d4fc36f
YanoTonny <yano.odhiambo@gmail.com>
2024-03-17 12:00:04 +0300

    First Hackathon challenge

---
+++ DartHackathon1/Function.dart
@@ -0,0 +1,21 @@
+void main() {
+  double num1 = 478.5;
+  double num2 = 67.5;
+
+  print("Addition: ${add(num1, num2)}");
+  print("Subtraction: ${subtract(num1, num2)}");
+  print("Multiplication: ${multiply(num1, num2)}");
+}
+
+double add(double a, double b) {
+  return a + b;
+}
+
+double subtract(double a, double b) {
+  return a - b;
+}
+
+double multiply(double a, double b) {
+  return a * b;
+
+}
---
+++ DartHackathon1/Program1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Tonny Odhiambo";
+  int age = 26;
+  String school = "Power Learn Academy";
+  String hobby = "Watching documentaries";
+
+  print("My name is $name. I am $age years old, studying at $school, and my hobby is $hobby.");
+}
