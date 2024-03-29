f538731a2aa005e53b790f50e4fa1e3ef643973f
YanoTonny <yano.odhiambo@gmail.com>
2024-03-29 22:22:57 +0300

    my first javascript code

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log("Hello, my name is Tonny Odhiambo Yano");
+console.log("I am a student at Power Learn academy");
+console.log("I am learning javascript");
+console.log("I'm hoping to become a software developer by the end of the year");
